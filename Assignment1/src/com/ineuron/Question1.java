ea978c1e31a39a13dea75ca3baf9232bebc9b832
thaheer-shaik <thahir074@gmail.com>
2022-10-23 22:50:00 +0530

    uploading assignment files

--- Assignment1/src/com/ineuron/Question1.java
+++ Assignment1/src/com/ineuron/Question1.java
@@ -1,9 +1,111 @@
 package com.ineuron;
 
+
 public class Question1 {
 
 	public static void main(String[] args) {
+		int n = 5;
 
+		for (int i = 0; i < n; i++) {
+			// I
+			for (int j = 0; j < n; j++) 
+			{
+				if (i == 0 || i == n - 1 || j == n / 2) 
+				{
+					System.out.print("*");
+				} 
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+
+			// N
+			for (int j = 0; j < n; j++) 
+			{
+				if (i == j || j == 0 || j == n - 1) 
+				{
+					System.out.print("*");
+				} 
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+
+			// E
+			for (int j = 0; j < n; j++) 
+			{
+				if (i == 0 || j == 0 || i == n / 2 || i == n - 1) 
+				{
+					System.out.print("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+
+			// U
+			for (int j = 0; j < n; j++) 
+			{
+				if (j == 0 || i == n - 1 || j == n - 1) 
+				{
+					System.out.print("*");
+				} 
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			
+			//R
+			for (int j = 0; j < n; j++) 
+			{
+				if (i==0 || (i>=n/2 && i==j) || j == 0 || (i<n/2 && j == n - 1)) 
+				{
+					System.out.print("*");
+				} 
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			
+			
+			// O
+			for (int j = 0; j < n; j++) 
+			{
+				if (i == 0 || j == 0 || i == n - 1 || j == n - 1) 
+				{
+					System.out.print("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+
+			// N
+			for (int j = 0; j < n; j++) 
+			{
+				if (i == j || j == 0 || j == n - 1) 
+				{
+					System.out.print("*");
+				} 
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
--- Assignment1/src/com/ineuron/Question4.java
+++ Assignment1/src/com/ineuron/Question4.java
@@ -2,7 +2,24 @@ package com.ineuron;
 
 public class Question4 {
 	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
+		int n=14;
+		
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j<=i-n/2 || j>=n+n/2-1-i)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
